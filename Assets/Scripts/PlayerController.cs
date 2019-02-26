ccc35727db1facd42a42e27a16ecb52430ce61b1
Christopher Powell <cpowel10@cnu.edu>
2019-02-25 20:14:22 -0500

    Improve Animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed;
 
     private Animator anim;
+    private bool playerMoving;
+    private Vector2 lastMove;
     private Rigidbody2D rbPlayer;
 
     // Start is called before the first frame update
@@ -19,16 +21,22 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        playerMoving = false;
+
         if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
         {
             //transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
             rbPlayer.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, rbPlayer.velocity.y);
+            playerMoving = true;
+            lastMove = new Vector2(Input.GetAxisRaw("Horizontal"),0f);
         }
 
         if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
         {
             //transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
             rbPlayer.velocity = new Vector2(rbPlayer.velocity.x , Input.GetAxisRaw("Vertical") * moveSpeed);
+            playerMoving = false;
+            lastMove = new Vector2(0f,Input.GetAxisRaw("Vertical"));
         }
 
         if (Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
@@ -43,5 +51,8 @@ public class PlayerController : MonoBehaviour
 
         anim.SetFloat("Move_X", Input.GetAxisRaw("Horizontal"));
         anim.SetFloat("Move_Y", Input.GetAxisRaw("Vertical"));
+        anim.SetBool("PlayerMoving",playerMoving);
+        anim.SetFloat("LastMoveX",lastMove.x);
+        anim.SetFloat("LastMoveY", lastMove.y);
     }
 }
