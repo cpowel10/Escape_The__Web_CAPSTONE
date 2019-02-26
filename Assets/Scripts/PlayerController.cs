c9a9cf76d89c77c0595a4c9283bf374313dd35b8
Christopher Powell <cpowel10@cnu.edu>
2019-02-25 19:32:24 -0500

    Colliders level 1 done

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,11 +7,13 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed;
 
     private Animator anim;
+    private Rigidbody2D rbPlayer;
 
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+        rbPlayer = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -19,12 +21,24 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
         {
-            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+            //transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+            rbPlayer.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, rbPlayer.velocity.y);
         }
 
         if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
         {
-            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+            //transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+            rbPlayer.velocity = new Vector2(rbPlayer.velocity.x , Input.GetAxisRaw("Vertical") * moveSpeed);
+        }
+
+        if (Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
+        {
+            rbPlayer.velocity = new Vector2(0f, rbPlayer.velocity.y);
+        }
+
+        if (Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5f)
+        {
+            rbPlayer.velocity = new Vector2(rbPlayer.velocity.x,0f);
         }
 
         anim.SetFloat("Move_X", Input.GetAxisRaw("Horizontal"));
