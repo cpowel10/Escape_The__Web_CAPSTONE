ca65dab1381d0a22a8fb96c6c416cde9c2f39502
Christopher Powell <cpowel10@cnu.edu>
2019-03-19 11:32:58 -0400

    Started battle scene and health system

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int startingHealth = 100;
+    public int currentHealth;
+
+    bool isDead;
+    bool isDamaged;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = startingHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDamaged)
+        {
+
+        }
+    }
+}
