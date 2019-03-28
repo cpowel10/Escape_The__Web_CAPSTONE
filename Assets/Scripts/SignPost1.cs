49dcbf733d420ee0b3863b7107a983e91babf9fa
Christopher Powell <cpowel10@cnu.edu>
2019-03-28 11:23:01 -0400

    Dialogue triggers: 70 minutes

---
+++ Assets/Scripts/SignPost1.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SignPost1 : MonoBehaviour
+{
+    public GameObject Signpost1;
+    public Text SP1Text;
+    public string SP1dialogue;
+    public bool SP1dialogueActive;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player in range");
+            //SP1dialogueActive = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player out of range");
+            //SP1dialogueActive = false;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Player out of range");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space") && SP1dialogueActive)
+        {
+            if (Signpost1.activeInHierarchy)
+            {
+                Signpost1.SetActive(false);
+            }
+            else
+            {
+                Signpost1.SetActive(true);
+                SP1Text.text = SP1dialogue;
+            }
+        }
+    }
+}
--- Assets/Scripts/UILevel1.cs
+++ Assets/Scripts/UILevel1.cs
@@ -6,42 +6,15 @@ using UnityEngine.UI;
 public class UILevel1 : MonoBehaviour
 {
     //public GameObject PuzzleGridEasy;
-    public GameObject Signpost1;
     public GameObject SPPuzzle1;
-    public Text SPText1;
     public GameObject NPC1;
-    public Text NPCText1;
-    public bool playerInRangeSign1;
     public GameObject PuzzleGridEasy;
-    private bool triggerEntered = false;
-
 
     // Start is called before the first frame update
     void Start()
     {
-        Signpost1.SetActive(false);
         NPC1.SetActive(false);
         PuzzleGridEasy.SetActive(false);
-    }
-
-    public void ShowSignPost1(Collider2D other)
-    {
-        //Signpost1.SetActive(true);
-        if (other.CompareTag("Player"))
-        {
-            playerInRangeSign1 = true;
-            //Debug.Log("Player in range");
-        }
-    }
-
-    public void HideSignPost1(Collider2D other)
-    {
-        //Signpost1.SetActive(false);
-        if (other.CompareTag("Player"))
-        {
-            playerInRangeSign1 = false;
-            //Debug.Log("Player left range");
-        }
     }
 
     public void ShowNPC1()
@@ -54,16 +27,12 @@ public class UILevel1 : MonoBehaviour
         NPC1.SetActive(false);
     }
 
-    public void OnTriggerEnter()
-    {
-        triggerEntered = true;
-    }
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("space") && triggerEntered == true)
-        {
-            PuzzleGridEasy.SetActive(true);
-        }
+        //if (Input.GetKeyDown("space"))
+        //{
+       //     PuzzleGridEasy.SetActive(true);
+        //}
     }
 }
